91f9a6f27338d5a593b7c69fd65adcf8273f45f4
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:57:53 +0200

    cambiamenti

--- ScontoNegozio/Program.cs
+++ ScontoNegozio/Program.cs
@@ -6,7 +6,22 @@ namespace ScontoNegozio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("inserisci l'importo");
+            double t = double.Parse(Console.ReadLine());
+            double s1 = (t * 10) / 100;
+            double s2 = (t * 20) / 100;
+            if (t <= 500)
+            {
+                double importo = t - s1;
+                Console.WriteLine($"l'importo totale è {importo} ");
+                Console.ReadLine();
+            }
+            if (t > 500)
+            {
+                double importo = t - s2;
+                Console.WriteLine($"l'importo totale è {importo} ");
+                Console.ReadLine();
+            }
         }
     }
 }
